8a98aa0ad1560251c27f9ad40b525449ce64e90a
prashantu <prashantu@codeblock.co.in>
2022-02-11 16:51:40 +0530

    dynamodb added.

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL, () => {
@@ -16,8 +18,9 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
-app.use("api/users", userRoute);
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(8800, () => {
-  console.log("backend is runningsjdhskjd");
+  console.log("backend is running");
 });
